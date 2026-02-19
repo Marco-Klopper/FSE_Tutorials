bb8523712edc55612ff06c46ec5752117197dec3
Marco-Klopper <110021680+Marco-Klopper@users.noreply.github.com>
2026-02-19 10:37:32 +0200

    Completed Tutorial 2: Implement transactions module

--- transactions.py
+++ transactions.py
@@ -22,7 +22,7 @@ def format_currency(amount: Decimal) -> str:
         >>> format_currency(Decimal("1234.56"))
         'R 1234.56'
     """
-    return ""
+    return f"{CURRENCY_SYMBOL} {amount:,.2f}"
 
 
 # TODO Implement the function below, if the transaction type is not valid raise a ValueError, if the amount is negative raise a ValueError
@@ -49,6 +49,17 @@ def add_transaction(
         >>> add_transaction(transactions, "Salary", Decimal("5000"), "income")
         [{'description': 'Salary', 'amount': Decimal('5000'), 'type': 'income'}]
     """
+
+    if transaction_type not in TRANSACTION_TYPES:
+        raise ValueError('ValueError: transaction_type is invalid. Ensure value is either "income" or "expense".')
+    
+    if amount < 0:
+        raise ValueError('ValueError: amount is invalid. Ensure value is positive')
+    
+
+    new_transaction = {'description': description, 'amount': Decimal(amount), 'type': transaction_type}
+    transactions.append(new_transaction)
+
     return transactions
 
 # TODO: Implement the function below
@@ -72,7 +83,17 @@ def calculate_balance(transactions: List[dict]) -> Decimal:
         >>> calculate_balance(transactions)
         Decimal('4000')
     """
-    return Decimal(0)
+    current_balance = 0
+
+    for transaction in transactions:
+        if transaction['type'] == 'income':
+            tnx_amount = transaction['amount'] 
+        if transaction['type'] == 'expense':
+            tnx_amount = -transaction['amount'] 
+
+        current_balance = current_balance + tnx_amount        
+
+    return Decimal(current_balance)
 
 # TODO: Implement the function below
 def get_income_total(transactions: List[dict]) -> Decimal:
@@ -85,7 +106,15 @@ def get_income_total(transactions: List[dict]) -> Decimal:
     Returns:
         The total income as a Decimal.
     """
-    return Decimal(0)
+
+    income_balance = 0
+
+    for transaction in transactions:
+        if transaction['type'] == 'income':
+            tnx_amount = transaction['amount']
+            income_balance = income_balance + tnx_amount 
+
+    return Decimal(income_balance)
 
 # TODO: Implement the function below
 def get_expense_total(transactions: List[dict]) -> Decimal:
@@ -98,7 +127,15 @@ def get_expense_total(transactions: List[dict]) -> Decimal:
     Returns:
         The total expenses as a Decimal.
     """
-    return Decimal(0)
+    
+    expense_balance = 0
+
+    for transaction in transactions:
+        if transaction['type'] == 'expense':
+            tnx_amount = transaction['amount']
+            expense_balance = expense_balance + tnx_amount 
+
+    return Decimal(expense_balance)
 
 # TODO: Implement the function below
 # NOTE: If the balance exceeds the budget limit, return False and a message indicating overspend in a tuple
@@ -120,7 +157,11 @@ def check_budget(balance: Decimal, budget_limit: Decimal) -> Tuple[bool, str]:
         >>> check_budget(Decimal("1500"), Decimal("1000"))
         (False, 'Budget exceeded! Overspent by R 500.00.')
     """
-    return (True, "")
+    if balance <= budget_limit:
+        return (True, f'Within budget. {format_currency(balance)} of {format_currency(budget_limit)} used.')
+    
+    if balance > budget_limit:
+        return (False, f'Budget exceeded! Overspent by {format_currency(balance - budget_limit)}.')
 
 # TODO: Implement the function below, however you see fit
 def display_transactions(transactions: List[dict]) -> None:
@@ -130,23 +171,31 @@ def display_transactions(transactions: List[dict]) -> None:
     Args:
         transactions: A list of transaction dictionaries.
     """
+    print("|     description     |       amount      |       type        |")
+    for tnx in transactions:
+        print(f"|     {tnx['description']}     |       {format_currency(tnx['amount'])}      |       {tnx['type']}        |")
+
     pass
 
 # Example usage
 if __name__ == "__main__":
     # Example usage
-    # transactions = []
-    # add_transaction(transactions, "Salary", Decimal("5000"), "income")
-    # add_transaction(transactions, "Groceries", Decimal("1500"), "expense")
-    # add_transaction(transactions, "Freelance", Decimal("2000"), "income")
-    # add_transaction(transactions, "Rent", Decimal("2500"), "expense")
+    transactions = []
+    add_transaction(transactions, "Salary", Decimal("5000"), "income")
+    add_transaction(transactions, "Groceries", Decimal("1500"), "expense")
+    add_transaction(transactions, "Freelance", Decimal("2000"), "income")
+    add_transaction(transactions, "Rent", Decimal("2500"), "expense")
 
-    # display_transactions(transactions)
+    display_transactions(transactions)
 
-    # balance = calculate_balance(transactions)
-    # print(f"\nCurrent Balance: {format_currency(balance)}")
+    balance = calculate_balance(transactions)
+    print(f"\nCurrent Balance: {format_currency(balance)}")
 
-    # budget_limit = Decimal("3000")
-    # within_budget, message = check_budget(balance, budget_limit)
-    # print(message)
+    budget_limit = Decimal("3000")
+    within_budget, message = check_budget(balance, budget_limit)
+    print(message)
     pass
+
+
+
+         
